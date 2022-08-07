101290eb43c27d17ae7ecd963b0c0ee94015b98e
PratikBaravkar <baravkarpratik5@gmail.com>
2022-08-07 11:29:25 +0530

    Project.sql file added by Pratik

---
+++ doc/Database Schema/project.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: project
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `Admin_ID` int NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_mame` varchar(100) DEFAULT NULL,
+  `phone` bigint DEFAULT NULL,
+  `mail_id` varchar(100) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Admin_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buyers`
+--
+
+DROP TABLE IF EXISTS `buyers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buyers` (
+  `BID` int NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_mame` varchar(100) DEFAULT NULL,
+  `phone` bigint DEFAULT NULL,
+  `mail_id` varchar(100) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`BID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buyers`
+--
+
+LOCK TABLES `buyers` WRITE;
+/*!40000 ALTER TABLE `buyers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buyers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `farmers`
+--
+
+DROP TABLE IF EXISTS `farmers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `farmers` (
+  `FID` int NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_mame` varchar(100) DEFAULT NULL,
+  `phone` bigint DEFAULT NULL,
+  `mail_id` varchar(100) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `Password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`FID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `farmers`
+--
+
+LOCK TABLES `farmers` WRITE;
+/*!40000 ALTER TABLE `farmers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `farmers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL,
+  `PID` int DEFAULT NULL,
+  `quantity` double DEFAULT NULL,
+  `total_amount` double DEFAULT NULL,
+  `FID` int DEFAULT NULL,
+  `BID` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `FID` (`FID`),
+  KEY `BID` (`BID`),
+  KEY `PID` (`PID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`FID`) REFERENCES `farmers` (`FID`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`BID`) REFERENCES `buyers` (`BID`),
+  CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`PID`) REFERENCES `products` (`PID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `PID` int NOT NULL,
+  `product_name` varchar(100) DEFAULT NULL,
+  `quantity` double DEFAULT NULL,
+  `expected_price` double DEFAULT NULL,
+  `FID` int DEFAULT NULL,
+  PRIMARY KEY (`PID`),
+  KEY `FID` (`FID`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`FID`) REFERENCES `farmers` (`FID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-06 12:32:10
