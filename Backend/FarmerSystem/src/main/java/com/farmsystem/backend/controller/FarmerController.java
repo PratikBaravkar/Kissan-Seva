1aef871c7cbfa7ce03e13ee93dfe4728b8aa13c8
unknown <dipakwaykar3120@gmail.com>
2022-09-09 10:38:55 +0530

    folder structure updated

---
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/FarmerController.java
@@ -0,0 +1,14 @@
+package com.farmsystem.backend.controller;
+
+public class FarmerController {
+
+	public FarmerController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/entity/Farmer.java
@@ -0,0 +1,14 @@
+package com.farmsystem.backend.entity;
+
+public class Farmer {
+
+	public Farmer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
