fc79a2ae59c648b57b8a4aa7493e307e803a91f7
unknown <dipakwaykar3120@gmail.com>
2022-09-09 23:24:08 +0530

    entity and repo updated by dipak

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/entity/Farmer.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/entity/Farmer.java
@@ -1,14 +1,35 @@
 package com.farmsystem.backend.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Farmer {
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="fid")
+	private int id;
+	
+	private String firstname;
+	private String lastname;
+	private String contact;
+	private String email;
+	private String password;
+	private String address;
+	private String user_name;
+	
+	
 	public Farmer() {
-		// TODO Auto-generated constructor stub
+		
+		
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
