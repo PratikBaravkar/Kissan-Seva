538b8f98cd8b1d23a6896f732a959495b16fe82b
PRAAAAA123 <prathameshdutondkar97@gmail.com>
2022-09-11 11:36:59 +0530

    Controller Updated

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/FarmerController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/FarmerController.java
@@ -2,13 +2,6 @@ package com.farmsystem.backend.controller;
 
 public class FarmerController {
 
-	public FarmerController() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
