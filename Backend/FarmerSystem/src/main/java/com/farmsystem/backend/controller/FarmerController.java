e4f27bdf8c3b4076d9ac4093b91c39cd0b9040bb
unknown <dipakwaykar3120@gmail.com>
2022-09-13 00:57:00 +0530

    Updated repo and controller by dipak

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/FarmerController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/FarmerController.java
@@ -1,7 +1,11 @@
 package com.farmsystem.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.farmsystem.backend.repository.FarmerRepo;
+
 public class FarmerController {
 
-	
+	@Autowired
+	FarmerRepo farmerRepo;
 
 }
--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/repository/ProductRepo.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/repository/ProductRepo.java
@@ -6,5 +6,10 @@ import com.farmsystem.backend.entity.Product;
 
 public interface ProductRepo extends JpaRepository<Product, Integer>
 {
-
+	@Query(nativeQuery = true,value="select * from products where crop=:crop")
+	public List<Product> findProduct(@Param("crop") String item);
+	
+	
+	@Query(nativeQuery = true,value="select quantity from products where fid=:fid And crop=:crop")
+	public double getQuantity(@Param("fid") int fid ,@Param("crop") String crop);
 }
