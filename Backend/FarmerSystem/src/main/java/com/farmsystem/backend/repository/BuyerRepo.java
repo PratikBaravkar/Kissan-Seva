90d40187bf4fda01b0fa8c393ab864a2d778d4ed
kajaltoraskar <kajaltoraskar99@gmail.com>
2022-09-13 14:31:03 +0530

    query inserted in buyer repo by kajal

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/repository/BuyerRepo.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/repository/BuyerRepo.java
@@ -1,11 +1,28 @@
 package com.farmsystem.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.farmsystem.backend.entity.Buyer;
 
 public interface BuyerRepo extends JpaRepository<Buyer, Integer> 
 {
+	@Query( nativeQuery = true,value="select bid from buyers where user_name=:user_name")
+	public int findByName(@Param("user_name") String user_name);
+	
+	@Modifying
+	@Transactional
+	@Query( nativeQuery = true,value="update buyers set firstname=:firstname where user_name=:user_name")
+	public void updateFirstName(@Param("firstname") String firstname,@Param("user_name") String user_name );
+	
+	@Modifying
+	@Transactional
+	@Query( nativeQuery = true,value="update buyers set lastname =:lastname where user_name=:user_name")
+	public void updateLastName(@Param("lastname") String lastname,@Param("user_name") String user_name );
+	
 	
 
 }
