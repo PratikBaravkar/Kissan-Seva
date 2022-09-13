14abeeb7f84fc777fda61f3387d8290199222502
PRAAAAA123 <prathameshdutondkar97@gmail.com>
2022-09-13 20:08:40 +0530

    Admin Controller Updated

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/AdminController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.farmsystem.backend.entity.Admin;
+import com.farmsystem.backend.entity.Farmer;
 import com.farmsystem.backend.repository.AdminRepo;
 import com.farmsystem.backend.repository.FarmerRepo;
 import com.farmsystem.backend.repository.ProductRepo;
@@ -44,6 +45,18 @@ public class AdminController {
 		
 		
 	}
+	
+	@PostMapping("/farmer-list")
+	public List<Farmer> getFarmers(){
+		
+		List<Farmer> farmerList=farmerRepo.findAll();
+		return farmerList;
+	}
+	
+
+	
+	
+	
 	
 	
 
