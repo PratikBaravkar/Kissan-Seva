7f7fe580785689f4460b1d515d0982ef5a5ed756
kajaltoraskar <kajaltoraskar99@gmail.com>
2022-09-14 13:37:06 +0530

    buyer cart controller updated by kajal

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerCartController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerCartController.java
@@ -1,14 +1,33 @@
 package com.farmsystem.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.farmsystem.backend.entity.BuyerCart;
+import com.farmsystem.backend.repository.BuyerCartRepo;
+import com.farmsystem.backend.repository.ProductRepo;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/buyer-cart")
 public class BuyerCartController 
 {
+	@Autowired
+	BuyerCartRepo buyercartRepo;
+	
+	@Autowired
+	ProductRepo productRepo;
+	
+	@PostMapping("/add")
+	public String regFarmer(@RequestBody BuyerCart cart)
+	{
+		buyercartRepo.save(cart);
+		return "added _successfully";
+	}
 	
 
 }
