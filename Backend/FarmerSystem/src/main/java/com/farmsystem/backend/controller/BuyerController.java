07d9ec04c7e1193d97f9231b082e5d06d6f9ebaa
kajaltoraskar <kajaltoraskar99@gmail.com>
2022-09-15 00:16:58 +0530

    Buyer controller updated

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
@@ -1,9 +1,14 @@
 package com.farmsystem.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.farmsystem.backend.entity.Buyer;
 import com.farmsystem.backend.repository.BuyerCartRepo;
 import com.farmsystem.backend.repository.BuyerRepo;
 import com.farmsystem.backend.repository.FarmerRepo;
@@ -28,6 +33,27 @@ public class BuyerController
 	
 	@Autowired
 	OrderRepo orderRepo;
+	
+	@PostMapping("/login")
+	public String loginUser(@RequestBody Buyer buyer)
+	{
+		System.out.println(buyer.getPassword());
+		System.out.println(buyer.getUser_name());
+		List<Buyer>buyerList = buyerRepo.findAll();
+		
+		String passMsg = "pass";
+		String failMsg = "fail";
+		
+		for(Buyer buyerobj : buyerList )
+		{
+			if(buyerobj.getUser_name().equals(buyer.getUser_name()) && buyerobj.getPassword().equals(buyer.getPassword()))
+			{
+				return passMsg ;
+			}
+		}
+		return failMsg;
+		
+	}
 
 	
 
