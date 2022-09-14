eda2dce59ff2673b50910467ad09755198f12452
kajaltoraskar <kajaltoraskar99@gmail.com>
2022-09-15 00:06:47 +0530

    Buyer controller updated by kajal

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
@@ -1,6 +1,33 @@
 package com.farmsystem.backend.controller;
 
-public class BuyerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farmsystem.backend.repository.BuyerCartRepo;
+import com.farmsystem.backend.repository.BuyerRepo;
+import com.farmsystem.backend.repository.FarmerRepo;
+import com.farmsystem.backend.repository.OrderRepo;
+import com.farmsystem.backend.repository.ProductRepo;
+
+@RestController
+@RequestMapping("/buyer")
+public class BuyerController 
+{
+	@Autowired
+	BuyerRepo buyerRepo;
+	
+	@Autowired
+	BuyerCartRepo buyercartRepo;
+	
+	@Autowired
+	FarmerRepo farmerRepo;
+	
+	@Autowired
+	ProductRepo productRepo;
+	
+	@Autowired
+	OrderRepo orderRepo;
 
 	
 
