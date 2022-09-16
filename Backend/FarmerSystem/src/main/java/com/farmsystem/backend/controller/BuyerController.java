1ff61a68c77450bb8207908d2d4744b0f9128dc2
kajaltoraskar <kajaltoraskar99@gmail.com>
2022-09-16 17:07:25 +0530

    buyer controller updated

--- Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
+++ Backend/FarmerSystem/src/main/java/com/farmsystem/backend/controller/BuyerController.java
@@ -1,8 +1,11 @@
 package com.farmsystem.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.farmsystem.backend.entity.Buyer;
 import com.farmsystem.backend.entity.BuyerCart;
+import com.farmsystem.backend.entity.Order;
 import com.farmsystem.backend.entity.Product;
 import com.farmsystem.backend.repository.BuyerCartRepo;
 import com.farmsystem.backend.repository.BuyerRepo;
@@ -90,6 +94,64 @@ public class BuyerController
 		return cartlist;
 	}
 	
+//	@PostMapping("/confirmed-orders")
+//	public List<Order> buyerCart(@RequestBody Buyer buyer) 
+//	{
+//	        
+//			System.out.println(buyer.getUser_name());
+//			String uname = buyer.getUser_name();
+//			
+//			int bid = buyerRepo.findByName(uname);
+//			
+//			List<Order> orderList = orderRepo.findByBId(bid);  
+//			
+//			return orderList;
+//		
+//	}
+//	
+//	@PostMapping("/addOrder")
+//	public String regFarmer(@RequestBody Order order) {
+//			
+//		String buyeruname = order.getBuyer().getUser_name();
+//		
+//		System.out.println(buyeruname);
+//		int bid = buyerRepo.findByName(buyeruname);
+//			order.getBuyer().setBid(bid);
+//			
+//			String farmername = order.getFarmer().getFirstname();
+//			System.out.println(farmername);
+//			int fid = farmerRepo.findByFid(farmername);
+//			order.getFarmer().setFid(fid);
+//		    orderRepo.save(order);
+//		     
+//		    return "added";
+//		    
+//	}
+//	
+	@GetMapping("/profile/{username}")
+	public Optional<Buyer> getBuyer(@PathVariable String username) 
+	{
+
+		
+		int bid = buyerRepo.findByName(username);
+			          
+		return buyerRepo.findById(bid);
+		    
+	}
+	
+	@GetMapping("/remove/{username}")
+	public String removeFarmer(@PathVariable String username) {
+
+		
+		try {
+			int bid = buyerRepo.findByName(username);
+			buyerRepo.deleteById(bid);
+			return "updated";
+		} catch (Exception e) {
+			return "fails";
+		}
+		    
+	}
 	
 	
 
